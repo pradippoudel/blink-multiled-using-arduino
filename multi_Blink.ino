eeed64ee1643c8b5be9d4aeea9280dfd407cbd21
Pradip Poudel <50986372+pradippoudel@users.noreply.github.com>
2019-08-02 22:38:44 +0545

    Add files via upload

---
+++ multi_Blink.ino
@@ -0,0 +1,28 @@
+
+int led1=13;
+int led2=12;
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+}
+
+// the loop function runs over and over again forever
+
+void loop() {
+  digitalWrite(led1, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(led1, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+
+
+  digitalWrite(12, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(12, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+
+  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(11, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
